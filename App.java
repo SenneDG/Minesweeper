8ac8e122fd4e11a6a57e48829c2a42003e083551
Sebastiaan Schoeters <sebastiaan.schoeters@student.kuleuven.be>
2022-03-14 15:15:08 +0100

    b

--- App.java
+++ App.java
@@ -6,17 +6,25 @@ import view.MinesweeperView;
 public class App {
     public static void main(String[] args) throws Exception {
         Minesweeper test = new Minesweeper();
-        test.startNewGame(Difficulty.MEDIUM);
+        test.startNewGame(Difficulty.EASY);
         MinesweeperView view = new MinesweeperView();
         //Uncomment the lines below once your game model code is ready; don't forget to import your game model 
-        //PlayableMinesweeper model = new Minesweeper();
+        PlayableMinesweeper model = new Minesweeper();
         
         
         /**
             Your code to bind your game model to the game user interface
         */
-        
-        
+        test.startNewGame(Difficulty.EASY);
+        int x = test.getHeight();
+        int y = test.getWidth();
+        //view.setGameModel(test);
+        view.notifyNewGame(x, y);
+
+
+
+
+
         //model.startNewGame(Difficulty.EASY);
     }
 }
